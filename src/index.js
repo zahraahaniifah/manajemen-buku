e35cdef1e98915726b52ea4334b032c2bc811931
Zahra Hanifah <zhanifah731@gmail.com>
2026-06-16 18:08:13 +0700

    add home route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express'); 
 const app = express();              
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 8080; 
 
+app.get('/', (req, res) => {
+    res.send('Selamat datang di API Manajemen Buku! Server berhasil berjalan.');
+});
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server berjalan di port ${PORT}`);
 });
