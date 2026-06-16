cca032345c379d1fb46b90f55d20d7158839eda4
Zahra Hanifah <zhanifah731@gmail.com>
2026-06-16 17:24:05 +0700

    update port railway

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-const app = require("./app");
+const PORT = process.env.PORT || 3000; 
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server berjalan di port ${PORT}`);
 });
